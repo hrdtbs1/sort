243083efc3041c7858a9827a7541f2188c411c53
tsubasa harada <harada.t.ak@m.titech.ac.jp>
2019-06-26 14:15:44 +0900

    c

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -15,43 +15,57 @@ void swap(int *p, int *q){
 }
 
 int quick_select(int A[], int n, int k){
-  int i, j, pivot;
-    if(n<5){
+    int i, j, pivot;
+    if(n<=5){
         pivot = A[0];
+        for(i = j = 1; i < n; i++){
+            if(A[i] <= pivot){
+                swap(A+i, A+j);
+                j++;
+            }
+        }
+        if(j == k+1) return pivot;
+        else if(j < k+1) return quick_select(A+j, n-j, k-j);
+        else return quick_select(A+1, j-1, k);
     }
-    else {
-        int h;
+    else{
+        int h,g,f;
         for(h = 0; h < n/5; h++){
             B[h]=quick_select(A+5*h, 5, 2);
         }
         if(n%5 != 0){
-            B[h+1]=quick_select(A+5*(h+1), n%5, (n%5)/2);
+            B[n/5]=quick_select(A+5*(n/5), n%5, (n%5)/2);
         }
         pivot = quick_select(B, (n+4)/5, (n+4)/10);
+        for(g=0;g<n;g++){
+            if(A[g]==pivot){
+                f=g;
+                g=n-1;
+            }
+         }
+        A[f] = A[0];
+        A[0] = pivot;
+        for(i = j = 1; i < n; i++){
+            if(A[i] <= pivot){
+                swap(A+i, A+j);
+                j++;
+            }
+        }
+        if(j == k+1) return pivot;
+        else if(j < k+1) return quick_select(A+j, n-j, k-j);
+        else return quick_select(A+1, j-1, k);
     }
-  for(i = j = 1; i < n; i++){
-    if(A[i] <= pivot){
-      swap(A+i, A+j);
-      j++;
-    }
-  }
-
-  if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
 }
 
-
-
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 17; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-  for(i=0;i<N;i++){
-    if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
-//    printf("%d th element is %d\n", i, quick_select(A, N, i));
-  }
+    int i;
+    A[0] = 0;
+    A[1] = 17; //原始元
+    for(i=2;i<N;i++){
+        A[i] = (long long int) A[i-1] * A[1] % N;
+    }
+    for(i=0;i<N;i++){
+       if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
+       //printf("%d th element is %d\n", i, quick_select(A, N, i));//
+    }
 }
